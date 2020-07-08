c9a80f008600fea5c2001cb3ee4ac2c14d9c337a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-07-08 19:02:48 +0300

    Fix asset manifest files

--- app/assets/config/admin/decidim_ideas_manifest.js
+++ app/assets/config/admin/decidim_ideas_manifest.js
@@ -1,4 +1,4 @@
-//= link decidim/ideas/admin/component_settings.js.js
+//= link decidim/ideas/admin/component_settings.js
 //= link decidim/ideas/admin/ideas.js
 //= link decidim/ideas/admin/ideas_form.js
 //= link decidim/ideas/admin/ideas_picker.js
