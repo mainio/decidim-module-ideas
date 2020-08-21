f2482b84d988ead1919b767180faedc0a79528a4
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-21 13:20:59 +0300

    Add missing event on idea answer

---
+++ app/events/decidim/ideas/evaluating_idea_event.rb
@@ -0,0 +1,11 @@
+# frozen-string_literal: true
+
+module Decidim
+  module Ideas
+    class EvaluatingIdeaEvent < Decidim::Events::SimpleEvent
+      def event_has_roles?
+        true
+      end
+    end
+  end
+end
