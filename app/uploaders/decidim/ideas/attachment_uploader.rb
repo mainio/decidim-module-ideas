f425c379530c8240075ed0b365ad6edfd775b41f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-09-09 19:10:57 +0300

    Fix issue with the ideas attachment uploader messing with its parent
    The attachment uploader needs to be fully redefined in order to
    avoid messing with the core attachment uploader.

--- app/uploaders/decidim/ideas/attachment_uploader.rb
+++ app/uploaders/decidim/ideas/attachment_uploader.rb
@@ -3,25 +3,37 @@
 module Decidim
   module Ideas
     # This class deals with uploading attachments related to an idea.
-    class AttachmentUploader < ::Decidim::AttachmentUploader
-      # Redefine the :strip processor for not to lose the exif data before
-      # the orientate processor has run.
-      processors.delete_if { |p| p[0] == :strip }
+    # We cannot extend Decidim::AttachmentUploader because we need to redefine
+    # some of the methods.
+    #
+    # Carrierwave does not allow to do this. In case we deleted e.g. versions or
+    # processors for redefinitions, they would also disappear from the parent
+    # uploader.
+    class AttachmentUploader < ::Decidim::ApplicationUploader
+      include CarrierWave::MiniMagick
 
+      process :set_content_type_and_size_in_model
+      process :validate_dimensions
       process :orientate, if: :image?
-      process :strip # Redefine the :strip processor AFTER :orientate
+      process :strip
 
-      # Redefine the :thumbnail version
-      versions.delete(:thumbnail)
       version :thumbnail, if: :image? do
         process resize_to_fill: [860, 340]
+      end
+
+      version :big, if: :image? do
+        process resize_to_limit: [nil, 1000]
       end
 
       version :main, if: :image? do
         process resize_to_fill: [1480, 740]
       end
 
-      private
+      protected
+
+      def extension_whitelist
+        %w(jpg jpeg gif png bmp pdf doc docx xls xlsx ppt ppx rtf txt odt ott odf otg ods ots)
+      end
 
       # Flips the image to be in correct orientation based on its Exif
       # orientation metadata.
@@ -31,6 +43,69 @@ module Decidim
           img = yield(img) if block_given?
           img
         end
+      end
+
+      # Strips out all embedded information from the image
+      def strip
+        return unless image?(self)
+
+        manipulate! do |img|
+          img.strip
+          img
+        end
+      end
+
+      # CarrierWave automatically calls this method and validates the content
+      # type fo the temp file to match against any of these options.
+      def content_type_whitelist
+        [
+          %r{image\/},
+          %r{application\/vnd.oasis.opendocument},
+          %r{application\/vnd.ms-},
+          %r{application\/msword},
+          %r{application\/vnd.ms-word},
+          %r{application\/vnd.openxmlformats-officedocument},
+          %r{application\/vnd.oasis.opendocument},
+          %r{application\/pdf},
+          %r{application\/rtf}
+        ]
+      end
+
+      # Checks if the file is an image based on the content type. We need this so
+      # we only create different versions of the file when it's an image.
+      #
+      # new_file - The uploaded file.
+      #
+      # Returns a Boolean.
+      def image?(new_file)
+        content_type = model.content_type || new_file.content_type
+        content_type.to_s.start_with? "image"
+      end
+
+      # Copies the content type and file size to the model where this is mounted.
+      #
+      # Returns nothing.
+      def set_content_type_and_size_in_model
+        model.content_type = file.content_type if file.content_type
+        model.file_size = file.size
+      end
+
+      # A simple check to avoid DoS with maliciously crafted images, or just to
+      # avoid reckless users that upload gigapixels images.
+      #
+      # See https://hackerone.com/reports/390
+      def validate_dimensions
+        return unless image?(self)
+
+        manipulate! do |image|
+          raise CarrierWave::IntegrityError, I18n.t("carrierwave.errors.image_too_big") if image.dimensions.any? { |dimension| dimension > max_image_height_or_width }
+
+          image
+        end
+      end
+
+      def max_image_height_or_width
+        8000
       end
     end
   end
