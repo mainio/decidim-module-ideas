4d705919e2411ec94bd2cf1ca2ccf69202fb3858
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-09-29 07:19:24 +0300

    Add the idea type to plans resource links

--- lib/decidim/ideas/engine.rb
+++ lib/decidim/ideas/engine.rb
@@ -183,6 +183,10 @@ module Decidim
           metric_operation.manager_class = "Decidim::Ideas::Metrics::IdeaFollowersMetricMeasure"
         end
       end
+
+      initializer "decidim_ideas.plans_integration" do
+        Decidim::Plans.api_resource_link_types << Decidim::Ideas::IdeaType
+      end
     end
   end
 end
