84cb82bf9cc0a4a8b4e38243578fa4475acfc8de
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-15 20:07:36 +0300

    Remove followers from all existing emendation records

---
+++ db/migrate/20201015170004_remove_followers_from_idea_emendations.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class RemoveFollowersFromIdeaEmendations < ActiveRecord::Migration[5.2]
+  def up
+    Decidim::Ideas::Idea.only_emendations.each do |idea|
+      idea.follows.destroy_all
+    end
+  end
+
+  def down; end
+end
