071280f0291454c578c952aa40fc6c3dc83cba0c
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-04-29 12:23:47 +0300

    Add IdeaParticipantsMetricMeasure spec

---
+++ spec/queries/decidim/ideas/metrics/idea_participants_metric_measure_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Ideas::Metrics::IdeaParticipantsMetricMeasure do
+  let(:day) { Time.zone.yesterday }
+  let(:organization) { create(:organization) }
+  let(:not_valid_resource) { create(:dummy_resource) }
+  let(:participatory_space) { create(:participatory_process, :with_steps, organization: organization) }
+
+  let(:ideas_component) { create(:idea_component, :published, participatory_space: participatory_space) }
+  let!(:idea) { create(:idea, published_at: day, component: ideas_component) }
+  let!(:old_idea) { create(:idea, published_at: day - 1.week, component: ideas_component) }
+  let!(:old_idea2) { create(:idea, published_at: day - 2.days, component: ideas_component) }
+
+  context "when executing class" do
+    it "fails to create object with an invalid resource" do
+      manager = described_class.new(day, not_valid_resource)
+
+      expect(manager).not_to be_valid
+    end
+
+    it "calculates" do
+      result = described_class.new(day, ideas_component).calculate
+
+      expect(result[:cumulative_users].count).to eq(3)
+      expect(result[:quantity_users].count).to eq(1)
+    end
+
+    it "does not found any result for past days" do
+      result = described_class.new(day - 1.month, ideas_component).calculate
+
+      expect(result[:cumulative_users].count).to eq(0)
+      expect(result[:quantity_users].count).to eq(0)
+    end
+  end
+end
