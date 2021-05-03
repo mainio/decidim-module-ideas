5c8d0b388384251ac47f015c02aba803aa0c69ba
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-05-03 17:49:35 +0300

    Edit idea attachments and attachment factory

--- spec/system/edit_idea_spec.rb
+++ spec/system/edit_idea_spec.rb
@@ -7,14 +7,13 @@ describe "User edits idea", type: :system do
 
   let(:organization) { create :organization }
   let(:participatory_process) { create :participatory_process, :with_steps, organization: organization }
-  let(:manifest_name) { "ideas" }
-  let(:manifest) { Decidim.find_component_manifest(manifest_name) }
+  let(:manifest) { Decidim.find_component_manifest("ideas") }
   let!(:user) { create :user, :confirmed, organization: organization }
   let!(:component) do
     create(:idea_component,
            :with_creation_enabled,
            :with_card_image_allowed,
-           :with_attachments_allowed,
+           :with_attachments_allowed, # Doesnt do anything?
            :with_geocoding_enabled,
            manifest: manifest,
            participatory_space: participatory_process)
@@ -28,15 +27,19 @@ describe "User edits idea", type: :system do
 
   before do
     component[:settings]["global"]["area_scope_parent_id"] = scope.id
+    settings = component.settings
+    settings.attachments_allowed = true
+    component.settings = settings
     component.save!
     login_as user, scope: :user
     visit_component
   end
 
   context "when user has created an idea" do
-    let!(:idea) { create :idea, users: [user], component: component, category: category }
+    let!(:idea) { create :idea, users: [user], component: component, category: category, area_scope_parent: scope }
     let(:new_title) { "Foo bar, much text here is" }
     let(:new_body) { "Veli kulta, veikkoseni, kaunis kasvinkumppalini! Lähe nyt kanssa laulamahan" }
+    let!(:subscope2) { create(:scope, parent: scope) }
 
     describe "edit idea" do
       before do
@@ -48,12 +51,101 @@ describe "User edits idea", type: :system do
       it "edits idea" do
         fill_in :idea_title, with: new_title
         fill_in :idea_body, with: new_body
-        select subscope.name["en"], from: :idea_area_scope_id
+        select subscope2.name["en"], from: :idea_area_scope_id
         select category.name["en"], from: :idea_category_id
         click_button "Save"
         expect(page).to have_content("Idea successfully updated")
         expect(Decidim::Ideas::Idea.last.title).to eq(new_title)
         expect(Decidim::Ideas::Idea.last.body).to eq(new_body)
+      end
+
+      context "when editing attachments", processing_uploads_for: Decidim::Ideas::AttachmentUploader do
+        let(:add_attachment_title) { ::Faker::Hipster.sentence }
+
+        it "adds image" do
+          click_button "Add an image for the idea"
+          attach_file(:idea_image_file, Decidim::Dev.asset("avatar.jpg"))
+          fill_in :idea_image_title, with: add_attachment_title
+          click_button "Add image"
+          click_button "Save"
+          expect(page).to have_content("Idea successfully updated")
+          expect(idea.attachments.last.content_type).to eq("image/jpeg")
+          expect(idea.attachments.last.title["en"]).to eq(add_attachment_title)
+        end
+
+        it "adds document" do
+          click_button "Add an attachment for the idea"
+          attach_file(:idea_attachment_file, Decidim::Dev.asset("Exampledocument.pdf"))
+          fill_in :idea_attachment_title, with: add_attachment_title
+          click_button "Add attachment"
+          click_button "Save"
+          expect(page).to have_content("Idea successfully updated")
+          expect(idea.attachments.last.content_type).to eq("application/pdf")
+          expect(idea.attachments.last.title["en"]).to eq(add_attachment_title)
+        end
+      end
+    end
+  end
+
+  context "when idea has image attached" do
+    let!(:idea2) do
+      create(:idea,
+             :with_photo,
+             users: [user],
+             title: idea2_title,
+             component: component,
+             category: category,
+             area_scope_parent: scope)
+    end
+    let(:idea2_title) { ::Faker::Hipster.sentence }
+    let(:weight) { 0 }
+
+    describe "remove image" do
+      before do
+        visit current_path
+        within ".card__header" do
+          click_link idea2_title
+        end
+        click_link "Edit idea"
+      end
+
+      it "remove image" do
+        click_button "Remove image"
+        click_link "OK"
+        click_button "Save"
+        expect(page).to have_content("Idea successfully updated")
+        expect(idea2.attachments.count).to eq(0)
+      end
+    end
+  end
+
+  context "when idea has document attached" do
+    let!(:idea3) do
+      create(:idea,
+             :with_document,
+             users: [user],
+             title: idea3_title,
+             component: component,
+             category: category,
+             area_scope_parent: scope)
+    end
+    let(:idea3_title) { ::Faker::Hipster.sentence }
+
+    describe "remove document" do
+      before do
+        visit current_path
+        within ".card__header" do
+          click_link idea3_title
+        end
+        click_link "Edit idea"
+      end
+
+      it "removes attached pdf" do
+        click_button "Remove attachment"
+        click_link "OK"
+        click_button "Save"
+        expect(page).to have_content("Idea successfully updated")
+        expect(idea3.attachments.count).to eq(0)
       end
     end
   end
